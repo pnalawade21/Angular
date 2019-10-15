ff5f91403269364811e34d029882af9d0e4f9e8c
Prashant <prashant.nalawade21@gmail.com>
2019-10-15 10:57:32 +0530

    Initial

---
+++ MyFirstAngularApp/MyFirstAngularApp/ClientApp/app/components/calculate/calculate.component.ts
@@ -0,0 +1,21 @@
+﻿import { Component } from '@angular/core';
+@Component({
+    selector: 'calculate',
+    templateUrl: './calculate.component.html',
+    styleUrls: ['./calculate.component.css']
+})
+export class CalculateComponent {
+    public firstNumber: number;
+    public secondNumber: number;
+    public result: number;
+
+    public add() {
+        this.result = this.firstNumber + this.secondNumber;
+    }
+    public sub() {
+        this.result = this.firstNumber - this.secondNumber;
+    }
+    public mult() {
+        this.result = this.firstNumber * this.secondNumber;
+    }
+}  

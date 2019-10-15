f52e33cf0fc8c2d017a0103d255d00fe2c770708
Prashant <prashant.nalawade21@gmail.com>
2019-10-15 15:54:19 +0530

    Added Customer component

---
+++ MyFirstAngularApp/MyFirstAngularApp/ClientApp/app/components/class/customer.ts
@@ -0,0 +1,9 @@
+﻿export class customer {
+    id: Number;
+    firstName: string;
+    lastName: string;
+    email: string;
+    gender: string;
+    city: string;
+    country: string;
+}
---
+++ MyFirstAngularApp/MyFirstAngularApp/ClientApp/app/components/customer/customer.component.ts
@@ -0,0 +1,17 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'customer',
+    templateUrl: './customer.component.html',
+    styleUrls: ['./customer.component.css']
+})
+
+/** customer component*/
+export class CustomerComponent {
+  
+    /** customer ctor */
+    constructor() {
+       
+
+    }
+}

f529113c22b7461e8da725c1cf9aea4893891acd
pnalawade <prashant.nalawade21@gmail.com>
2019-12-07 14:18:52 +0530

    Corrected API errors

--- AngularAPI/connection/connect.js
+++ AngularAPI/connection/connect.js
@@ -1,18 +1,12 @@
-"use strict";
-exports.__esModule = true;
 var sql = require("mssql");
-var Connect = /** @class */ (function () {
-    function Connect() {
-    }
-    Connect.prototype.GetSqlConnection = function () {
-        var conn = new sql.ConnectionPool({
-            user: "PNTest",
-            password: "$Abc1234",
-            server: "P703648-W10",
-            database: "TestAngularDB"
-        });
-        return conn;
-    };
-    return Connect;
-}());
-exports.Connect = Connect;
+var connect = function(){
+    var conn = new sql.ConnectionPool({
+        user: "PNTest",
+        password: "$Abc1234",
+        server: "P703648-W10",
+        database: "TestAngularDB"
+    });
+    return conn;
+};
+
+module.exports = connect;
--- AngularAPI/server.js
+++ AngularAPI/server.js
@@ -1,39 +1,22 @@
-"use strict";
-exports.__esModule = true;
-var bodyParser = require("body-parser");
-var moment = require("moment");
-var express = require("express");
-var Server = /** @class */ (function () {
-    function Server() {
-        this.cors = require('cors');
-        this.productController = require('./controller/ProductController');
-        // Create expressjs application
-        this.app = express();
-        // create application/x-www-form-urlencoded parser
-        this.app.use(bodyParser.urlencoded({ 'extended': true }));
-        // create application/json parser
-        this.app.use(bodyParser.json());
-        this.app.use(this.cors());
-        // Route our backend calls
-        this.app.use("/api/products", this.productController);
-        // Start the server on the provided port
-        this.app.listen(this.port, function () {
-            var datetime = new Date();
-            var message = "Server runnning on Port:- " + this.port + "Started at :- " + datetime;
-            console.log(message);
-        });
-        // Catch errors
-        this.app.on('error', function (error) {
-            console.error(moment().format(), 'ERROR', error);
-        });
-        process.on('uncaughtException', function (error) {
-            console.error(moment().format(), error);
-        });
-    }
-    Server.bootstrap = function () {
-        return new Server();
-    };
-    return Server;
-}());
-var server = Server.bootstrap();
-exports["default"] = server.app;
+
+var express = require('express');
+//https://github.com/expressjs/cors
+var cors = require("cors");
+var app = express();
+var port = process.env.port || 1337;
+var productController = require('./controller/ProductController')();
+
+var bodyParser = require('body-parser');
+// create application/x-www-form-urlencoded parser
+app.use(bodyParser.urlencoded({extended:true}));
+// create application/json parser
+app.use(bodyParser.json());
+app.use(cors());
+app.use("/api/products",productController);
+
+app.listen(port, function(){
+    var datetime = new Date();
+    var message = "Server runnning on Port:- " + port + "Started at :- " + datetime;
+
+    console.log(message);
+})
